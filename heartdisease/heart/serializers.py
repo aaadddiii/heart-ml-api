7993e6b301b9483c88fb43311b61a0a6ddf55be5
aaadddiii <adithya.kurien@gmail.com>
2020-09-06 10:47:45 +0530

    deploy heroku

--- heartdisease/heart/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from . models import employees
-
-class employeesSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = employees
-        fields = '__all__'
